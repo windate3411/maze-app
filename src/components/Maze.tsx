e80dd3665cf0836846298437c178089b2d4cb847
Danny <dannywang.tw@ffn.com>
2024-06-28 11:46:03 +0800

    small ui improvment

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -9,28 +9,59 @@ interface MazeProps {
 export const Maze: React.FC<MazeProps> = ({ maze, solutionPath }) => {
   const isSolutionCell = (x: number, y: number) =>
     solutionPath.some((point) => point.x === x && point.y === y);
+
+  const borderStyle = (cell: MazeCell, x: number, y: number) => {
+    const mazeWidth = maze[0].length;
+    const mazeHeight = maze[0].length;
+    const isStartOrEndAtTop = (cell.isStart || cell.isEnd) && y === 0;
+    const isStartOrEndAtRight =
+      (cell.isStart || cell.isEnd) && x === mazeWidth - 1;
+    const isStartOrEndAtBottom =
+      (cell.isStart || cell.isEnd) && y === mazeHeight - 1;
+    const isStartOrEndAtLeft = (cell.isStart || cell.isEnd) && x === 0;
+    const borders = {
+      top: cell.walls.top && !isStartOrEndAtTop ? "" : "border-t-0",
+      right: cell.walls.right && !isStartOrEndAtRight ? "" : "border-r-0",
+      bottom: cell.walls.bottom && !isStartOrEndAtBottom ? "" : "border-b-0",
+      left: cell.walls.left && !isStartOrEndAtLeft ? "" : "border-l-0",
+    };
+    return `${borders.top} ${borders.right} ${borders.bottom} ${borders.left}`;
+  };
+
+  const arrowDirection = (
+    cell: MazeCell,
+    x: number,
+    y: number,
+    mazeWidth: number,
+    mazeHeight: number
+  ): string => {
+    if (cell.isStart || cell.isEnd) {
+      if ((y === 0 && cell.isStart) || (cell.isEnd && y === mazeHeight - 1))
+        return "↓";
+      if ((y === mazeHeight - 1 && cell.isStart) || (cell.isEnd && y === 0))
+        return "↑";
+      if ((x === 0 && cell.isStart) || (cell.isEnd && x === mazeWidth - 1))
+        return "→";
+      if ((x === mazeWidth - 1 && cell.isStart) || (cell.isEnd && x === 0))
+        return "←";
+    }
+    return "";
+  };
   return (
     <div className="grid grid-cols-20">
       {maze.map((row, y) =>
         row.map((cell, x) => (
           <div
             key={`cell-${x}-${y}`}
-            className={`w-5 h-5  border-[#DD2244] box-border border ${
-              !cell.walls.top ? "border-t-0" : ""
-            } ${!cell.walls.right ? "border-r-0" : ""} ${
-              !cell.walls.bottom ? "border-b-0" : ""
-            } ${!cell.walls.left ? "border-l-0" : ""} ${
-              isSolutionCell(x, y) ? "bg-green-500" : "bg-transparent"
-            }`}
+            className={`w-5 h-5  border-[#DD2244] box-border border ${borderStyle(
+              cell,
+              x,
+              y
+            )} ${isSolutionCell(x, y) ? "bg-[#32CD32]" : "bg-transparent"}`}
           >
-            {cell.isStart && (
-              <div className="text-xs w-full h-full grid place-items-center bg-green-500">
-                S
-              </div>
-            )}
-            {cell.isEnd && (
-              <div className="text-xs w-full h-full grid place-items-center bg-red-500">
-                E
+            {(cell.isStart || cell.isEnd) && (
+              <div className="text-xs w-full h-full grid place-items-center text-white">
+                {arrowDirection(cell, x, y, maze[0].length, maze.length)}
               </div>
             )}
           </div>
